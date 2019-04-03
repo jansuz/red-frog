f528e248a50086abb96a126ec99bd2df76cf7c01
Jolyon Ansuz <jansuz@users.noreply.github.com>
2019-04-03 11:58:43 +1100

    Add files via upload

---
+++ Parse_Pwned_XLSX.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+oF=$(basename $1 .xlsx)_"$2".csv
+uF=$(basename $1 .xlsx)_"$2"_Users.csv
+
+xlsx2csv $1 | grep "$2" | cut -f1 -d, > "$uF"
+xlsx2csv $1 | grep "$2" > "$oF"
+
+wc -l "$oF"
+
+echo -e "Please find attached the Parsed HIBP File for '$2'.\n" > body.txt
+cat "$uF" >> body.txt
+echo -e "\nPlease do not reply to this email. It is automatically generated.\n" >> body.txt
+cat ~/.signature >> body.txt
+
+cat body.txt | mutt -a "$oF" -s "$oF" -- jansuz@une.edu.au	
+
+rm "body.txt" "$oF" "$uF"
